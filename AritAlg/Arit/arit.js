099a45bdb83e984de6e06a7a204bd09bf8ff6d34
bookdoestudante-pixel <bookdoestudante@gmail.com>
2026-05-22 00:41:51 -0400

    Add files via upload

---
+++ AritAlg/Arit/arit.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const btnVoltar = document.getElementById("btnVoltar");
+  const btnHome = document.getElementById("btnHome");
+
+  btnVoltar.addEventListener("click", () => {
+    if (history.length > 1) {
+      history.back();
+    } else {
+      window.location.href = "../../matt.html";
+    }
+  });
+
+  btnHome.addEventListener("click", () => {
+    window.location.href = "/menu.html";
+  });
+
+  document.querySelectorAll("[data-go]").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const destino = btn.dataset.go;
+
+      if (destino) {
+        window.location.href = destino;
+      }
+    });
+  });
+
+});
